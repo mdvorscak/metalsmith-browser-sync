8ef910f4743c29a5d83535a6c4c81100bd7c1c11
Mike Dvorscak <mike.dvorscak@gmail.com>
2015-04-11 11:10:30 -0500

    Adding testing scripts to gulpfile + dev dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,21 @@
 var gulp = require('gulp');
+var jasmine = require('gulp-jasmine');
+var istanbul = require('gulp-istanbul');
 
+gulp.task('test', function (done) {
+    gulp.src('index.js')
+        .pipe(istanbul())
+        .pipe(istanbul.hookRequire())
+        .on('finish', function () {
+                gulp.src('test/*.js')
+                    .pipe(jasmine())
+                    .pipe(istanbul.writeReports({
+                              dir: './coverage'
+                          }))
+                    .on('end', done);
+            });
+});
+
+gulp.task('tdd', function(){
+   gulp.watch(['index.js', 'test/*.js'], ['test']);
+});
