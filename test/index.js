181a6e344c1b36030ed8a8df3e0654844e0ae2f6
Mike Dvorscak <mike.dvorscak@gmail.com>
2015-04-11 11:30:05 -0500

    Unit tests stubbed out

---
+++ test/index.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Mike Dvorscak on 4/11/15.
+ */
+var plugin = require('../index.js');
+
+describe('metalsmith-browser-sync', function(){
+   it('should launch a static server', function(){
+
+   });
+
+    it('should allow me to specify the static folder that is served', function(){
+
+    });
+
+    it('rebuild when a watched file changes', function(){
+
+    });
+
+    it('should allow me to specify the watched files', function(){
+
+    });
+
+    it('should not attempt to launch multiple browser-sync servers (after each file change)', function(){
+
+    });
+});
