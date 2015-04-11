d01933420672362b1607bc72886f603c53b34de2
Mike Dvorscak <mike.dvorscak@gmail.com>
2015-04-11 16:44:06 -0500

    Added dev dependencies for tests, need to add fixtures

--- test/index.js
+++ test/index.js
@@ -1,26 +1,45 @@
 /**
  * Created by Mike Dvorscak on 4/11/15.
  */
-var plugin = require('../index.js');
+var proxyquire      = require('proxyquire');
+var Metalsmith      = require('metalsmith');
+var bsCalledWith    = {};
+var browserSyncMock = {
+    browserSync : function (options) {
+        bsCalledWith = options;
+    }
+};
+var plugin          = proxyquire('../index.js', {'browser-sync' : browserSyncMock.browserSync});
 
-describe('metalsmith-browser-sync', function(){
-   it('should launch a static server', function(){
-
-   });
-
-    it('should allow me to specify the static folder that is served', function(){
+describe('metalsmith-browser-sync', function () {
+    it('should launch a static server', function (done) {
+        spyOn(browserSyncMock, 'browserSync');
+        Metalsmith(__dirname)
+            .use(plugin)
+            .build(function (err) {
+                       if (err) {
+                           console.error(err);
+                       }
+                       expect(browserSyncMock.browserSync).toHaveBeenCalled();
+                       expect(bsCalledWith.server).toBeDefined();
+                       done();
+                   });
 
     });
 
-    it('rebuild when a watched file changes', function(){
+    it('should allow me to specify the static folder that is served', function () {
 
     });
 
-    it('should allow me to specify the watched files', function(){
+    it('rebuild when a watched file changes', function () {
 
     });
 
-    it('should not attempt to launch multiple browser-sync servers (after each file change)', function(){
+    it('should allow me to specify the watched files', function () {
+
+    });
+
+    it('should not attempt to launch multiple browser-sync servers (after each file change)', function () {
 
     });
 });
