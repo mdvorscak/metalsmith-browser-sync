b41708a54feb0dc5892194bf52450288f1d665b2
Mike Dvorscak <mike.dvorscak@gmail.com>
2015-04-11 20:33:57 -0500

    Added tests for debug messages and file watching

--- test/index.js
+++ test/index.js
@@ -10,10 +10,23 @@ var browserSyncMock = {
         bsCalledWith = options;
     }
 };
-var plugin          = proxyquire('../index.js', {'browser-sync' : browserSyncMock.browserSync});
+
+var debugMessage;
+var debugMock = {
+    debug : function (namespace) {
+        return function(message){
+            debugMessage = message;
+        };
+    }
+};
+var plugin    = proxyquire('../index.js', {
+    'browser-sync' : browserSyncMock.browserSync,
+    'debug'        : debugMock.debug
+});
 
 describe('metalsmith-browser-sync', function () {
     var build;
+    var metalsmith;
 
     function buildErrorHandler (err) {
         expect('build').toBe('successful');
@@ -21,9 +34,10 @@ describe('metalsmith-browser-sync', function () {
     }
 
     beforeEach(function () {
-        build = function builder(plugin){
+        debugMessage = '';
+        build = function builder (plugin) {
             return new Promise(function (resolve, reject) {
-                Metalsmith('test')
+                metalsmith = Metalsmith('test')
                     .source('fixtures')
                     .use(plugin)
                     .build(function (err) {
@@ -43,43 +57,117 @@ describe('metalsmith-browser-sync', function () {
         build(plugin()).then(assertions).catch(buildErrorHandler).then(done);
     });
 
-    it('should use the build directory by default', function(done){
-        function assertions () {
-            expect(bsCalledWith.server).toBe('build');
-        }
+    describe('options', function () {
+        it('should use the build directory by default', function (done) {
+            function assertions () {
+                expect(bsCalledWith.server).toBe('build');
+            }
 
-        build(plugin()).then(assertions).catch(buildErrorHandler).then(done);
+            build(plugin()).then(assertions).catch(buildErrorHandler).then(done);
+        });
+
+        it('should allow me to specify the static folder that is served', function (done) {
+            function assertions () {
+                expect(bsCalledWith.server).toBe('test');
+            }
+
+            build(plugin({server : 'test'})).then(assertions).catch(buildErrorHandler).then(done);
+        });
+
+        it('should use default watched files, if no files are provided', function (done) {
+            function assertions () {
+                expect(bsCalledWith.files).toEqual(["src/**/*.md", "templates/**/*.hbs"]);
+            }
+
+            build(plugin()).then(assertions).catch(buildErrorHandler).then(done);
+        });
+
+        it('should allow me to specify the watched files', function (done) {
+            function assertions () {
+                expect(bsCalledWith.files).toEqual(['test/*.test']);
+            }
+
+            build(plugin({files : ['test/*.test']})).then(assertions).catch(buildErrorHandler).then(done);
+        });
     });
 
-    it('should allow me to specify the static folder that is served', function (done) {
-        function assertions () {
-            expect(bsCalledWith.server).toBe('test');
-        }
-
-        build(plugin({server:'test'})).then(assertions).catch(buildErrorHandler).then(done);
-    });
-
-    it('rebuild when a watched file changes', function () {
+    it('should not effect other plugins', function(){
 
     });
 
-    it('should use default watched files, if no files are provided', function (done) {
-        function assertions () {
-            expect(bsCalledWith.files).toEqual(["src/**/*.md", "templates/**/*.hbs"]);
-        }
+    describe('metalsmith mock tests', function(){
+        var rawPlugin, metalsmithMock;
+        beforeEach(function(){
+            rawPlugin = plugin();
+            metalsmithMock = {
+                build : function(callback){
+                    callback();
+                },
+                plugins : []
+            };
+        });
+        it('should not attempt to launch multiple browser-sync servers (after each file change)', function (done) {
+            spyOn(browserSyncMock, 'browserSync').and.callThrough();
+            spyOn(metalsmithMock, 'build').and.callThrough();
 
-        build(plugin()).then(assertions).catch(buildErrorHandler).then(done);
-    });
+            var callCount = 0;
+            function simulateFileChange () {
+                callCount++;
+                var fnToCall = callCount > 1 ? assertions : simulateFileChange;
+                bsCalledWith.middleware(null, null, fnToCall);
+            }
 
-    it('should allow me to specify the watched files', function (done) {
-        function assertions () {
-            expect(bsCalledWith.files).toEqual(['test/*.test']);
-        }
+            function assertions () {
+                expect(metalsmithMock.build.calls.count()).toBe(2);
+                expect(browserSyncMock.browserSync.calls.count()).toBe(1);
+                done();
+            }
+            rawPlugin(null, metalsmithMock, simulateFileChange);
+        });
 
-        build(plugin({files: ['test/*.test']})).then(assertions).catch(buildErrorHandler).then(done);
-    });
+        it('rebuild when a watched file changes', function (done) {
 
-    it('should not attempt to launch multiple browser-sync servers (after each file change)', function () {
+            spyOn(metalsmithMock, 'build').and.callThrough();
 
+            function simulateFileChange () {
+                bsCalledWith.middleware(null, null, assertions);
+            }
+
+            function assertions () {
+                expect(debugMessage).toBe('Build successful');
+                expect(metalsmithMock.build.calls.count()).toBe(1);
+                done();
+            }
+            rawPlugin(null, metalsmithMock, simulateFileChange);
+        });
+
+        describe('debug messages', function(){
+            it('should be successful if there were no issues', function(done){
+                function simulateFileChange () {
+                    bsCalledWith.middleware(null, null, assertions);
+                }
+
+                function assertions () {
+                    expect(debugMessage).toBe('Build successful');
+                    done();
+                }
+                rawPlugin(null, metalsmithMock, simulateFileChange);
+            });
+
+            it('should give the error message when the build has an error', function(done){
+                metalsmithMock.build = function(callback){
+                    callback('test error');
+                };
+                function simulateFileChange () {
+                    bsCalledWith.middleware(null, null, assertions);
+                }
+
+                function assertions () {
+                    expect(debugMessage).toBe('test error');
+                    done();
+                }
+                rawPlugin(null, metalsmithMock, simulateFileChange);
+            });
+        });
     });
 });
